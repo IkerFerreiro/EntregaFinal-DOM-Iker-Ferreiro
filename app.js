adf7cab7102ceb4e6c161c105f72378339ab90a5
IkerFerreiro <ikerferreiroclase@gmail.com>
2025-04-11 16:30:54 +0200

    V0.2

---
+++ app.js
@@ -0,0 +1,62 @@
+const products = [
+    {
+      name: 'HP Essentials 255 G8 AMD',
+      price: 289,
+      seller: 'PcComponentes',
+      image: 'https://juguettos.com/3485234-thickbox_default/hasbro-gaming-monopoly-banca-virtual-juego-de-mesa.jpg'
+    },
+    {
+        name: 'HP Essentials 255 G8 AMD',
+        price: 289,
+        seller: 'PcComponentes',
+        image: 'https://juguettos.com/hasbro-monopoly/94710-hasbro-gaming-monopoly-banca-virtual-juego-de-mesa-5010996333711.html'
+      },
+      {
+        name: 'HP Essentials 255 G8 AMD',
+        price: 289,
+        seller: 'PcComponentes',
+        image: 'https://thumb.pccomponentes.com/w-300-300/articles/1005/10057282/1639-hp-essential-255-g8-amd-3020e-8gb-256gb-ssd-156.jpg'
+      },
+      {
+        name: 'HP Essentials 255 G8 AMD',
+        price: 289,
+        seller: 'PcComponentes',
+        image: 'https://thumb.pccomponentes.com/w-300-300/articles/1005/10057282/1639-hp-essential-255-g8-amd-3020e-8gb-256gb-ssd-156.jpg'
+      },
+      {
+        name: 'HP Essentials 255 G8 AMD',
+        price: 289,
+        seller: 'PcComponentes',
+        image: 'https://thumb.pccomponentes.com/w-300-300/articles/1005/10057282/1639-hp-essential-255-g8-amd-3020e-8gb-256gb-ssd-156.jpg'
+      },
+      {
+        name: 'HP Essentials 255 G8 AMD',
+        price: 289,
+        seller: 'PcComponentes',
+        image: 'https://thumb.pccomponentes.com/w-300-300/articles/1005/10057282/1639-hp-essential-255-g8-amd-3020e-8gb-256gb-ssd-156.jpg'
+      },
+      {
+        name: 'HP Essentials 255 G8 AMD',
+        price: 289,
+        seller: 'PcComponentes',
+        image: 'https://thumb.pccomponentes.com/w-300-300/articles/1005/10057282/1639-hp-essential-255-g8-amd-3020e-8gb-256gb-ssd-156.jpg'
+      },
+      {
+        name: 'HP Essentials 255 G8 AMD',
+        price: 289,
+        seller: 'PcComponentes',
+        image: 'https://thumb.pccomponentes.com/w-300-300/articles/1005/10057282/1639-hp-essential-255-g8-amd-3020e-8gb-256gb-ssd-156.jpg'
+      },
+      {
+        name: 'HP Essentials 255 G8 AMD',
+        price: 289,
+        seller: 'PcComponentes',
+        image: 'https://thumb.pccomponentes.com/w-300-300/articles/1005/10057282/1639-hp-essential-255-g8-amd-3020e-8gb-256gb-ssd-156.jpg'
+      },
+      {
+        name: 'HP Essentials 255 G8 AMD',
+        price: 289,
+        seller: 'PcComponentes',
+        image: 'https://thumb.pccomponentes.com/w-300-300/articles/1005/10057282/1639-hp-essential-255-g8-amd-3020e-8gb-256gb-ssd-156.jpg'
+      }
+];
